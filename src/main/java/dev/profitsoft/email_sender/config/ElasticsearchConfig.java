c3a84e6096f013310d3d70e76e78907509243c65
olivertate9 <kolomytsev95@gmail.com>
2024-06-11 19:24:30 +0300

    removed unused imports

--- src/main/java/dev/profitsoft/email_sender/config/ElasticsearchConfig.java
+++ src/main/java/dev/profitsoft/email_sender/config/ElasticsearchConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.lang.NonNullApi;
 
 /**
  * Configuration class for Elasticsearch setup.
